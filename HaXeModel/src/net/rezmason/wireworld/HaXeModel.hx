2d60ad43de75bcf96e37952571cd45572d3fca88
jeremy.sachs <jeremy.sachs@bdfb5009-3bc9-0ba2-8a8f-d2eee3557079>
2010-08-06 09:19:58 +0000

    Working out how to integrate haXe code into the project. No luck so far.

--- HaXeModel/src/net/rezmason/wireworld/HaXeModel.hx
+++ HaXeModel/src/net/rezmason/wireworld/HaXeModel.hx
@@ -1,60 +1,127 @@
 package net.rezmason.wireworld;
 
+// IMPORT STATEMENTS
 import flash.display.BitmapData;
 import flash.display.Graphics;
 import flash.events.EventDispatcher;
 import flash.events.IEventDispatcher;
+import flash.geom.Rectangle;
 
 class HaXeModel extends EventDispatcher, implements IModel {
 	
-	public var base(default, null):BitmapData;
-	public var baseGraphics(default, null):Graphics;
-	public var credit:String;
-	public var generation(default, null):Float;
-	public var headData(default, null):BitmapData;
-	public var headGraphics(default, null):Graphics;
-	public var height(default, null):Int;
-	public var historyGraphics(default, null):Graphics;
-	public var implementsOverdrive(default, null):Bool;
-	public var overdriveActive:Bool;
-	public var tailData(default, null):BitmapData;
-	public var tailGraphics(default, null):Graphics;
-	public var width(default, null):Int;
-	public var wireData(default, null):BitmapData;
-	public var wireGraphics(default, null):Graphics;
-	
+	// PRIVATE PROPERTIES
 	private var nodeTable:Array<HaXeNode>;
 	
-	public function new() {
+	// CONSTRUCTOR
+	public function new():Void {
 		super();
 		nodeTable = [];
 	}
 	
-	public function getState(__x:Int,  __y:Int):UInt {
+	// GETTERS & SETTERS
+	
+	public var width(get_width, never):Int;
+	private function get_width():Int {
 		return 0;
 	}
 	
-	public function init(txt:String,  ?isMCell:Bool):Void {
+	public var height(get_height, never):Int;
+	private function get_height():Int {
+		return 0;
+	}
+	
+	public var wireData(get_wireData, never):BitmapData;
+	private function get_wireData():BitmapData {
+		return null;
+	}
+	
+	public var headData(get_headData, never):BitmapData;
+	private function get_headData():BitmapData {
+		return null;
+	}
+	
+	public var tailData(get_tailData, never):BitmapData;
+	private function get_tailData():BitmapData {
+		return null;
+	}
+	
+	public var credit(get_credit, never):String;
+	private function get_credit():String {
+		return "";
+	}
+	
+	public var generation(get_generation, never):Float;
+	private function get_generation():Float {
+		return 0;
+	}
+	
+	public var baseGraphics(get_baseGraphics, never):Graphics;
+	private function get_baseGraphics():Graphics {
+		return null;
+	}
+	
+	public var wireGraphics(get_wireGraphics, never):Graphics;
+	private function get_wireGraphics():Graphics {
+		return null;
+	}
+	
+	public var headGraphics(get_headGraphics, never):Graphics;
+	private function get_headGraphics():Graphics {
+		return null;
+	}
+	
+	public var tailGraphics(get_tailGraphics, never):Graphics;
+	private function get_tailGraphics():Graphics {
+		return null;
+	}
+	
+	public var heatGraphics(get_heatGraphics, never):Graphics;
+	private function get_heatGraphics():Graphics {
+		return null;
+	}
+	
+	public var implementsOverdrive(get_implementsOverdrive, never):Bool;
+	private function get_implementsOverdrive():Bool {
+		return false;
+	}
+	
+	public var overdriveActive(get_overdriveActive, set_overdriveActive):Bool;
+	private function get_overdriveActive():Bool {
+		return false;
+	}
+	private function set_overdriveActive(value:Bool):Bool {
+		return value;
+	}
+	
+	// PUBLIC METHODS
+	
+	public function init(txt:String, ?isMCell:Bool = false):Void {
 		
 	}
 	
-	public function refreshAll():Void {
-		
-	}
-	
-	public function refreshHistory():Void {
-		
-	}
-	
-	public function refreshImage():Void {
-		
-	}
-	
-	public function reset():Void {
+	public function setBounds(t:Int, l:Int, b:Int, r:Int):Void {
 		
 	}
 	
 	public function update():Void {
 		
 	}
+	
+	public function refresh(?flags:Int = 0):Void {
+		
+	}
+	
+	public function getState(__x:Int, __y:Int):UInt {
+		return 0;
+	}
+	
+	public function reset():Void {
+		
+	}
+	
+	public function eraseRect(rect:Rectangle):Void {
+		
+	}
+	
+	// PRIVATE METHODS
 }
--- HaXeModel/src/net/rezmason/wireworld/IModel.hx
+++ HaXeModel/src/net/rezmason/wireworld/IModel.hx
@@ -3,29 +3,29 @@ package net.rezmason.wireworld;
 import flash.display.BitmapData;
 import flash.display.Graphics;
 import flash.events.IEventDispatcher;
+import flash.geom.Rectangle;
 
 extern interface IModel implements IEventDispatcher {
-	var base(default, null):BitmapData;
-	var baseGraphics(default, null):Graphics;
-	var credit:String;
-	var generation(default, null):Float;
-	var headData(default, null):BitmapData;
-	var headGraphics(default, null):Graphics;
-	var height(default, null):Int;
-	var historyGraphics(default, null):Graphics;
-	var implementsOverdrive(default, null):Bool;
-	var overdriveActive:Bool;
-	var tailData(default, null):BitmapData;
-	var tailGraphics(default, null):Graphics;
-	var width(default, null):Int;
-	var wireData(default, null):BitmapData;
-	var wireGraphics(default, null):Graphics;
-	
-	function getState(__x:Int,  __y:Int):UInt;
-	function init(txt:String,  ?isMCell:Bool):Void;
-	function refreshAll():Void;
-	function refreshHistory():Void;
-	function refreshImage():Void;
-	function reset():Void;
-	function update():Void;
+	public var width(get_width, never):Int;
+	public var height(get_height, never):Int;
+	public var wireData(get_wireData, never):BitmapData;
+	public var headData(get_headData, never):BitmapData;
+	public var tailData(get_tailData, never):BitmapData;
+	public var credit(get_credit, never):String;
+	public var generation(get_generation, never):Float;
+	public var baseGraphics(get_baseGraphics, never):Graphics;
+	public var wireGraphics(get_wireGraphics, never):Graphics;
+	public var headGraphics(get_headGraphics, never):Graphics;
+	public var tailGraphics(get_tailGraphics, never):Graphics;
+	public var heatGraphics(get_heatGraphics, never):Graphics;
+	public var implementsOverdrive(get_implementsOverdrive, never):Bool;
+	public var overdriveActive(get_overdriveActive, set_overdriveActive):Bool;
+
+	public function init(txt:String, ?isMCell:Bool = false):Void;
+	public function setBounds(t:Int, l:Int, b:Int, r:Int):Void;
+	public function update():Void;
+	public function refresh(?flags:Int = 0):Void;
+	public function getState(__x:Int, __y:Int):UInt;
+	public function reset():Void;
+	public function eraseRect(rect:Rectangle):Void;
 }
