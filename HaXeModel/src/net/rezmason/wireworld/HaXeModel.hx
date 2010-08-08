32b269b52073a187a267710a4fd0c43d3d98d745
jeremy.sachs <jeremy.sachs@bdfb5009-3bc9-0ba2-8a8f-d2eee3557079>
2010-08-08 17:47:46 +0000

    Still trying to figure out how to integrate haXe classes.

--- HaXeModel/src/net/rezmason/wireworld/HaXeModel.hx
+++ HaXeModel/src/net/rezmason/wireworld/HaXeModel.hx
@@ -3,125 +3,61 @@ package net.rezmason.wireworld;
 // IMPORT STATEMENTS
 import flash.display.BitmapData;
 import flash.display.Graphics;
+import flash.events.Event;
 import flash.events.EventDispatcher;
 import flash.events.IEventDispatcher;
+import flash.events.TimerEvent;
 import flash.geom.Rectangle;
+import flash.utils.Timer;
 
 class HaXeModel extends EventDispatcher, implements IModel {
 	
 	// PRIVATE PROPERTIES
 	private var nodeTable:Array<HaXeNode>;
+	private var wait:Timer;
 	
 	// CONSTRUCTOR
 	public function new():Void {
 		super();
 		nodeTable = [];
+		wait = new Timer(1000, 1);
+		wait.addEventListener(TimerEvent.TIMER, done);
 	}
 	
 	// GETTERS & SETTERS
 	
-	public var width(get_width, never):Int;
-	private function get_width():Int {
-		return 0;
-	}
-	
-	public var height(get_height, never):Int;
-	private function get_height():Int {
-		return 0;
-	}
-	
-	public var wireData(get_wireData, never):BitmapData;
-	private function get_wireData():BitmapData {
-		return null;
-	}
-	
-	public var headData(get_headData, never):BitmapData;
-	private function get_headData():BitmapData {
-		return null;
-	}
-	
-	public var tailData(get_tailData, never):BitmapData;
-	private function get_tailData():BitmapData {
-		return null;
-	}
-	
-	public var credit(get_credit, never):String;
-	private function get_credit():String {
-		return "";
-	}
-	
-	public var generation(get_generation, never):Float;
-	private function get_generation():Float {
-		return 0;
-	}
-	
-	public var baseGraphics(get_baseGraphics, never):Graphics;
-	private function get_baseGraphics():Graphics {
-		return null;
-	}
-	
-	public var wireGraphics(get_wireGraphics, never):Graphics;
-	private function get_wireGraphics():Graphics {
-		return null;
-	}
-	
-	public var headGraphics(get_headGraphics, never):Graphics;
-	private function get_headGraphics():Graphics {
-		return null;
-	}
-	
-	public var tailGraphics(get_tailGraphics, never):Graphics;
-	private function get_tailGraphics():Graphics {
-		return null;
-	}
-	
-	public var heatGraphics(get_heatGraphics, never):Graphics;
-	private function get_heatGraphics():Graphics {
-		return null;
-	}
-	
-	public var implementsOverdrive(get_implementsOverdrive, never):Bool;
-	private function get_implementsOverdrive():Bool {
-		return false;
-	}
-	
-	public var overdriveActive(get_overdriveActive, set_overdriveActive):Bool;
-	private function get_overdriveActive():Bool {
-		return false;
-	}
-	private function set_overdriveActive(value:Bool):Bool {
-		return value;
-	}
+	public var baseGraphics(default,null) : flash.display.Graphics;
+	public var credit(default,null) : String;
+	public var generation(default,null) : Float;
+	public var headData(default,null) : flash.display.BitmapData;
+	public var headGraphics(default,null) : flash.display.Graphics;
+	public var heatGraphics(default,null) : flash.display.Graphics;
+	public var height(default,null) : Int;
+	public var implementsOverdrive(default,null) : Bool;
+	public var overdriveActive : Bool;
+	public var tailData(default,null) : flash.display.BitmapData;
+	public var tailGraphics(default,null) : flash.display.Graphics;
+	public var width(default,null) : Int;
+	public var wireData(default,null) : flash.display.BitmapData;
+	public var wireGraphics(default,null) : flash.display.Graphics;
 	
 	// PUBLIC METHODS
 	
-	public function init(txt:String, ?isMCell:Bool = false):Void {
-		
-	}
-	
-	public function setBounds(t:Int, l:Int, b:Int, r:Int):Void {
-		
-	}
-	
-	public function update():Void {
-		
-	}
-	
-	public function refresh(?flags:Int = 0):Void {
-		
-	}
-	
-	public function getState(__x:Int, __y:Int):UInt {
+	public function eraseRect(rect : flash.geom.Rectangle) : Void;
+	public function getState(__x : Int, __y : Int) : UInt {
 		return 0;
 	}
-	
-	public function reset():Void {
-		
+	public function init(txt : String, ?isMCell : Bool) : Void {
+		wait.start();
 	}
-	
-	public function eraseRect(rect:Rectangle):Void {
-		
-	}
+	public function refresh(?flags : Int) : Void;
+	public function reset() : Void;
+	public function setBounds(t : Int, l : Int, b : Int, r : Int) : Void;
+	public function update() : Void;
 	
 	// PRIVATE METHODS
+	
+	private function done(event:Event):Void {
+		dispatchEvent(new Event(Event.COMPLETE));
+	}
 }
